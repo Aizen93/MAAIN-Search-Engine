7fb4973259ade4d2cfaef66e0057d73cad7a2ac8
aouessar <oussama--93@live.fr>
2020-02-04 15:56:26 +0100

    Initial Commit

---
+++ TP1/Collecteur/src/Collecteur.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Oussama, Lilya
+ */
+public class Collecteur {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
