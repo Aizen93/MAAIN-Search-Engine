c8525a37b8f2272a993c0d30fa130f1c16d4e2af
AOUESSAR oussama <aouessar@informatique.univ-paris-diderot.fr>
2020-03-27 04:33:15 +0100

    Authors and architecture files + new version

---
+++ AUTHORS.md
@@ -0,0 +1,4 @@
+# Members :
+-	Aouessar Oussama -  21104903 - @aouessar
+-	Medjadi Mohamed Ilyess - num etu - @medjadi
+-	Melila Lilya  - num etu - @melila
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Dépendances requises
 var express = require('express');
+var bodyParser = require('body-parser');
 var serv = express();
+serv.use(bodyParser.urlencoded({ extended: true }));
 serv.set('view engine', 'ejs');
 
 // Démarrage du serveur
@@ -9,6 +11,19 @@ serv.listen(8080, function() {
     console.log("Server started");
 });
 
+var x = 0;
+
 serv.get("/", function (req, res) {
+    
     res.render("pages/index.ejs");
+});
+
+
+serv.post("/", function (req, res) {
+    console.log("---------------------------");
+    console.log(req.body);
+    console.log("---------------------------");
+    var search = String(req.body.search);
+    console.log(search);
+    res.render("pages/index", {data: req.body});
 });
