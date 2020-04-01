03ac818190f96945dbb2e0e38f528313ff3d12b8
AOUESSAR oussama <aouessar@informatique.univ-paris-diderot.fr>
2020-04-01 06:38:08 +0200

    Function to read heavy files like frwiki

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Dépendances requises
 var express = require('express');
+var fs = require('fs');
 var bodyParser = require('body-parser');
 var serv = express();
 serv.use(bodyParser.urlencoded({ extended: true }));
@@ -11,13 +12,51 @@ serv.listen(8080, function() {
     console.log("Server started");
 });
 
-var x = 0;
+//-------------------------------------------------------------------//
+//-------------------------------------------------------------------//
+//-------------------------------------------------------------------//
+
+function printData(data) {
+    // Data will be null when it reaches the end.
+    if (!data) return;
+    
+    // type of data = Buffer
+    var stringData = data.toString('utf8', 0, data.length);
+    console.log(stringData.length);
+    //console.log(stringData);
+  }
+
+function readFileStream(fileName, cb, options) {
+    return new Promise(function(resolve, reject) {
+        var readable = fs.createReadStream(fileName, options);
+
+        readable.on('data', function(data) {
+            cb(data);
+        })
+
+        readable.on('end', function() {
+            resolve();
+        })
+
+        readable.on('error', function(err) {
+            return reject(err);
+        })
+    })
+}
+
+readFileStream('./test.txt', printData, {highWaterMark: 512 * 1024}).then(function() {
+    console.log("\nDone reading");
+  }).catch(function(err) {
+    console.log(err);
+  })
+
+//-------------------------------------------------------------------//
+//-------------------------------------------------------------------//
+//-------------------------------------------------------------------//
 
 serv.get("/", function (req, res) {
-    
     res.render("pages/index.ejs");
 });
-
 
 serv.post("/", function (req, res) {
     var search = String(req.body.search);
