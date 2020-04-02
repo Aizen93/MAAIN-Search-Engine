d0ef8d04428800b428458b6b8548919c40747944
AOUESSAR oussama <aouessar@informatique.univ-paris-diderot.fr>
2020-04-02 05:41:24 +0200

    Corpus generation v1.0

--- index.js
+++ index.js
@@ -2,56 +2,79 @@
 var express = require('express');
 var fs = require('fs');
 var bodyParser = require('body-parser');
+var saxParser = require('sax').createStream(true);
+var saxPath = require('saxpath');
 var serv = express();
 serv.use(bodyParser.urlencoded({ extended: true }));
 serv.set('view engine', 'ejs');
 
 // Démarrage du serveur
-serv.use(express.static(__dirname + '/public'));
+/*serv.use(express.static(__dirname + '/public'));
 serv.listen(8080, function() {
     console.log("Server started");
-});
+});*/
 
 //-------------------------------------------------------------------//
-//-------------------------------------------------------------------//
+//------------------ CORPUS GENERATION CODE -------------------------//
 //-------------------------------------------------------------------//
 
-function printData(data) {
-    // Data will be null when it reaches the end.
-    if (!data) return;
+var dataURL = './frwiki-20190120-pages-articles.xml';
+//var dataURL = './public/data/frwiki-debut.xml';
+var corpusURL = "./corpus.xml";
+var NUMBER_OF_PAGE = 600000;
+var count = 0;
+var regexp = [/intelligence artificielle/i, /informati*/i, /nouvelle technologie/i, /robot/i];
+
+function filtrer(elmt) {
+    var contains = false;
+    regexp.forEach(
+        function (exp) {
+            if(elmt.match(exp)) {
+                contains = true;
+                return;
+            }
+        }
+    );
+    return contains;
+};
+
+function parseXML(bigfrwiki, corpusoutput) {
+    var fileStream = fs.createReadStream(bigfrwiki);
+    var corpus_stream = fs.createWriteStream(corpusoutput, {flags:'a'});
+    var streamer = new saxPath.SaXPath(saxParser, '//page');
     
-    // type of data = Buffer
-    var stringData = data.toString('utf8', 0, data.length);
-    console.log(stringData.length);
-    //console.log(stringData);
-  }
-
-function readFileStream(fileName, cb, options) {
-    return new Promise(function(resolve, reject) {
-        var readable = fs.createReadStream(fileName, options);
-
-        readable.on('data', function(data) {
-            cb(data);
-        })
-
-        readable.on('end', function() {
-            resolve();
-        })
-
-        readable.on('error', function(err) {
-            return reject(err);
-        })
-    })
+    corpus_stream.write('<corpus version="0.10" xml:lang="fr">');
+    streamer.on('match', function(xml) {
+        if(count < NUMBER_OF_PAGE){
+            if(filtrer(xml)){
+                corpus_stream.write("\n\t" + xml);
+                count++;
+            }
+        }else{
+            fileStream.close();
+            corpus_stream.write('\n</corpus>');
+            console.log("Corpus created succefully !!");
+            console.log("Total pages : " + count);
+        }
+    });
+    streamer.on('error', function(){
+        console.log("Error parsing "+ bigfrwiki +" file !");
+    });
+    fileStream.on('error', function(){
+        console.log("Error parsing "+ bigfrwiki +" file !!!");
+    });
+    fileStream.on('end', function(){
+        corpus_stream.write('\n</corpus>');
+        console.log("Corpus created succefully !!!");
+        console.log("Total pages : " + count);
+    });
+    fileStream.pipe(saxParser);
 }
 
-readFileStream('./test.txt', printData, {highWaterMark: 512 * 1024}).then(function() {
-    console.log("\nDone reading");
-  }).catch(function(err) {
-    console.log(err);
-  })
+parseXML(dataURL, corpusURL);
 
 //-------------------------------------------------------------------//
-//-------------------------------------------------------------------//
+//---------------- END OF CORPUS GENERATION CODE --------------------//
 //-------------------------------------------------------------------//
 
 serv.get("/", function (req, res) {
