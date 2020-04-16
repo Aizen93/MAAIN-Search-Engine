b1e816592a6495363edc358cc5205d2a4ccf3b86
Lilya Melila <lilya.m@live.fr>
2020-04-16 04:12:24 +0200

    mise à jour graph.js

--- graph.js
+++ graph.js
@@ -2,41 +2,59 @@
 var fs = require('fs');
 var saxParser = require('sax').createStream(true);
 var saxPath = require('saxpath');
-
 //-------------------------------------------------------------------//
 //-------------------------- CODE GRAPH -----------------------------//
 //-------------------------------------------------------------------//
 var fileStream = fs.createReadStream("./corpus.xml");
-var corpus_stream = fs.createWriteStream('./graph.xml', {flags:'a'});
-
+var corpus_stream = fs.createWriteStream('./graph.xml');
+var string = "";
+var count = 1;
 function graph(){
   var start = new Date().getTime();
   var words_occurence = new  Map();
   var streamer = new saxPath.SaXPath(saxParser, '//page');
   var finish = false;
 
+  var i = 0;
   corpus_stream.write('<graph version="0.10" xml:lang="fr">\n\t');
 
   streamer.on('match', function(xml) {
-    var str = xml
-    var id = getFromBetween.get(str,"<id>","</id");
-    //var id = str.split('<id>').pop().split('</id>')[0];
-    var title = str.split('<title>').pop().split('</title>')[0];
-    var text = str.split('<text xml:space="preserve">').pop().split('</text>')[0];
+    console.log(count);
+    count++;
+    corpus_stream.write("<page>\n\t");
+    var string = xml
+    var id = xml.split('<id>')[1].split('</id>')[0];
+    var title = xml.split('<title>').pop().split('</title>')[0];
+    corpus_stream.write("   <id>"+id+"</id>\n\t");
+    corpus_stream.write("   <title>"+title+"</title>\n\t");
+    corpus_stream.write("   <link>\n\t");
 
-    corpus_stream.write("<id>"+id[0]+"</id>\n\t");
-    corpus_stream.write("<title>"+title+"</title>\n\t");
-    var liens = [];
-    liens = str.split('[[').pop().split(']]');
-    //getFromBetween.get(text,"[[","]]");
+    var buffer1 = string.substr(0,30000);
+    var buffer2 = "";
+    string.replace(buffer1, "");
+    sub1 = '[[';
+    sub2 = ']]';
+    while (string.length > 0){
+      if(!(buffer1.indexOf('[[') < 0 || buffer1.indexOf(']]') < 0)) {
+        var SP = buffer1.indexOf(sub1)+sub1.length;
+        var string1 = buffer1.substr(0,SP);
+        var string2 = buffer1.substr(SP);
+        var TP = string1.length + string2.indexOf(sub2);
+        var res = buffer1.substring(SP,TP);
+        var tmp = res.split('|')[0];
+        //console.log(tmp);
+        corpus_stream.write("        <title>"+tmp+"</title>\n\t");
+        buffer1 = buffer1.substr(SP,buffer1.length);
+      }else{
+        buffer2 = string.substr(0,30000);
+        string = string.replace(buffer2,"");
+        buffer1.concat(buffer2);
+      }
+    }
+    corpus_stream.write("   </link>\n\t");
+    corpus_stream.write("</page>\n\t");
 
-    //console.log('taille tmp : '+tmp.length);
-    liens.forEach((item) => {
-      //var word = item.split(':')[0];
-      if(!item.match('Categorie')) corpus_stream.write("     <title>"+item.split('|')[0]+"</title>\n\t");
-    });
-    corpus_stream.write("</link>\n\t");
-    console.log('titre : '+title+', id : '+id[0]);
+    console.log('titre : '+title+', id : '+id);
   });
   fileStream.on('error', function(){
     console.log("Error parsing file !!!");
@@ -51,47 +69,7 @@ function graph(){
   fileStream.pipe(saxParser);
 
 }
-
 graph();
-
-var getFromBetween = {
-  results:[],
-  string:"",
-  count:0,
-  getFromBetween:function (sub1,sub2) {
-    if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
-    var SP = this.string.indexOf(sub1)+sub1.length;
-    var string1 = this.string.substr(0,SP);
-    var string2 = this.string.substr(SP);
-    var TP = string1.length + string2.indexOf(sub2);
-    return this.string.substring(SP,TP);
-  },
-  removeFromBetween:function (sub1,sub2) {
-    if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
-    var removal = sub1+this.getFromBetween(sub1,sub2)+sub2;
-    this.string = this.string.replace(removal,"");
-  },
-  getAllResults:function (sub1,sub2) {
-    if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return;
-    //console.log(id);
-    if(sub1 === '<id>' && this.count == 0){
-      var id = this.getFromBetween(sub1,sub2);
-      this.count++;
-      this.results.push(id);
-      return;
-    }
-    this.removeFromBetween(sub1,sub2);
-    if(this.string.indexOf(sub1) > -1 && this.string.indexOf(sub2) > -1)  this.getAllResults(sub1,sub2);
-    else return;
-  },
-  get:function (string,sub1,sub2) {
-    this.results = [];
-    this.count = 0;
-    this.string = string;
-    this.getAllResults(sub1,sub2);
-    return this.results;
-  }
-};
 
 //-------------------------------------------------------------------//
 //------------------------ END CODE GRAPH ---------------------------//
