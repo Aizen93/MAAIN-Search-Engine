a56e791b05f586728c8e0a1cb300dd0396d266b0
Lilya Melila <lilya.m@live.fr>
2020-04-20 22:47:03 +0200

    maj front + graph

--- graph.js
+++ graph.js
@@ -5,19 +5,14 @@ var saxPath = require('saxpath');
 //-------------------------------------------------------------------//
 //-------------------------- CODE GRAPH -----------------------------//
 //-------------------------------------------------------------------//
-var fileStream = fs.createReadStream("./corpus.xml");
-var corpus_stream = fs.createWriteStream('./graph.xml');
+
 var string = "";
 var count = 1;
-function graph(){
-  var start = new Date().getTime();
-  var words_occurence = new  Map();
+
+function parser(corpus_stream) {
+
   var streamer = new saxPath.SaXPath(saxParser, '//page');
-  var finish = false;
-
-  var i = 0;
-  corpus_stream.write('<graph version="0.10" xml:lang="fr">\n\t');
-
+  var fileStream = fs.createReadStream("./corpus.xml");
   streamer.on('match', function(xml) {
     console.log(count);
     count++;
@@ -34,7 +29,7 @@ function graph(){
     string.replace(buffer1, "");
     sub1 = '[[';
     sub2 = ']]';
-    while (string.length > 0){
+    while (string.length > 0 || !(buffer1.indexOf('[[') < 0 || buffer1.indexOf(']]') < 0)){
       if(!(buffer1.indexOf('[[') < 0 || buffer1.indexOf(']]') < 0)) {
         var SP = buffer1.indexOf(sub1)+sub1.length;
         var string1 = buffer1.substr(0,SP);
@@ -60,6 +55,10 @@ function graph(){
     }
     corpus_stream.write("   </link>\n\t");
     corpus_stream.write("</page>\n\t");
+    if(count == 627287) {
+      corpus_stream.write('\n</graph>')
+      fileStream.close();
+    };
     //console.log('titre : '+title+', id : '+id);
   });
   fileStream.on('error', function(){
@@ -68,18 +67,34 @@ function graph(){
 
   fileStream.on('end', function(){
     fileStream.close();
-    corpus_stream.write('\n</graph>');
-    var end = new Date().getTime();
-    var time = end - start;
-    console.log(end - start);
     console.log("Graph created successfully !!!");
-    corpus_stream.close();
   });
   fileStream.pipe(saxParser);
-
 }
+
+function graph(){
+  var corpus_stream = fs.createWriteStream('./graph.xml');
+  var start = new Date().getTime();
+  var words_occurence = new  Map();
+
+  corpus_stream.write('<graph version="0.10" xml:lang="fr">\n\t');
+
+  parser(corpus_stream);
+
+  var end = new Date().getTime();
+  var time = end - start;
+  console.log(end - start);
+}
+
 graph();
 
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
 //-------------------------------------------------------------------//
 //------------------------ END CODE GRAPH ---------------------------//
 //-------------------------------------------------------------------//
--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var serv = express();
+var test = require('./test');
+var Node = require('./Node.js');
+
 serv.use(bodyParser.urlencoded({ extended: true }));
 serv.set('view engine', 'ejs');
 var fs = require('fs');
@@ -9,14 +12,30 @@ var saxParser = require('sax').createStream(true);
 var saxPath = require('saxpath');
 var removePunctuation = require('remove-punctuation');
 // Démarrage du serveur
+// graph.graph();;
+var dictionary_array;
+var graph_array;
 serv.use(express.static(__dirname + '/public'));
 serv.listen(8080, function() {
   console.log("Server started");
+  //dictionary_array = dictionary();
 });
 
 serv.get("/", function (req, res) {
-  res.render("pages/index.ejs");
-    dictionary();
+  //graph_array = [];
+  //graph_array.push(new Node(10,"Algorithme"));
+  //graph_array.push(new Node(11,"Algorithmique"));
+
+  if(graph_array.length != undefined) {
+    console.log("taille du graphe : "+graph_array.length);
+    res.render("pages/index.ejs", {list:graph_array});
+  }
+  else{
+    console.log("Graphe en cours de récupération...");
+    console.log(test.graph_array);
+    res.render("pages/index.ejs", {list:null});
+  }
+    //dictionary();
 });
 
 serv.post("/", function (req, res) {
@@ -24,3 +43,9 @@ serv.post("/", function (req, res) {
   console.log(upper_noaccent(search));
   res.render("pages/index", {data: req.body});
 });
+
+function main() {
+  graph_array = test.graph();
+  console.log("indice 0 : "+graph_array[0]);
+}
+main();
