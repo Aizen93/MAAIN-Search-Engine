bd8a2ce601d9ac40a31bdf3ce53dbb6a706fda77
MEDJADI ilyesse mohamed <medjadi@informatique.univ-paris-diderot.fr>
2020-04-26 23:51:28 +0200

    (clean)

--- Node.js
+++ Node.js
@@ -19,18 +19,4 @@ class Node {
     this.links.push(str);
   }
 }
-var list = new Array();
-list.push(10, "Algorithme");
-list.push(21, "Autre titre...");
-
-console.log("--------------------- list --------------------");
-console.log(list);
-console.log("-------------------- object -------------------");
-console.log(list[1]);
-if(list[1] == "Algorithme") console.log("id : "+list[1]);
-var title;
-list.forEach((item, i) => {
-  if(item == 10) title = list[i+1];
-});
-console.log(title);
 module.exports = Node
